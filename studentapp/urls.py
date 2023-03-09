b6a78ed6fbd7e35e2d76b08b9caca0cf3f30b183
S-Sadi <80355714+S-Sadi@users.noreply.github.com>
2023-03-09 10:10:38 +0600

    Update urls.py
    Now root URL is login and register URL is add-student

--- studentapp/urls.py
+++ studentapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.register , name="register"),
-    path("login", views.login, name="login"),
+    path("add-student", views.register , name="register"),
+    path("", views.login, name="login"),
     path("dashboard/<str:pk>/", views.dashboard, name='dashboard'),
     path("all-student", views.all_student, name="all-student"),
     path("student-details/<int:pk>", views.student_details, name='student-details')
